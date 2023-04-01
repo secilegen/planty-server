3e40ca574fd1bb1dbb4c15e2e353d3fb92eae112
Nadine Machmeier <info@nadinemachmeier.com>
2023-04-01 15:18:44 +0100

    cloudinary plants BE

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      // credentials: true, // added with CLOUDINARY
+      credentials: true, // added with CLOUDINARY
       origin: [FRONTEND_URL]
     })
   );
